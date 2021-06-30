a65e4df2421a81dabbd799ab6da0c151d53d6849
Nidhi-Joshi15 <nidhi@appemirates.com>
2021-06-30 15:12:46 +0400

    added test scenario

--- AppDemoTests/AppDemoTests.swift
+++ AppDemoTests/AppDemoTests.swift
@@ -37,5 +37,29 @@ class AppDemoTests: XCTestCase {
         }
         waitForExpectations(timeout: 5, handler: nil)
     }
+    func testCallToiTunesCompletes() {
+        let session: URLSession = URLSession(configuration: .default)
+        
+        let url = URL(string: API.baseUrl)
+        
+        let promise = expectation(description: "Completion handler invoked")
+        var statusCode: Int?
+        var responseError: Error?
+        
+        // when
+        let dataTask = session.dataTask(with: url!) { data, response, error in
+            statusCode = (response as? HTTPURLResponse)?.statusCode
+            responseError = error
+            // 2
+            promise.fulfill()
+        }
+        dataTask.resume()
+        // 3
+        wait(for: [promise], timeout: 5)
+        
+        // then
+        XCTAssertNil(responseError)
+        XCTAssertEqual(statusCode, 200)
+    }
 
 }
